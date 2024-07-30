75b88e6a51f73c0da617a49b0e54bed0fbf0b61c
alvaaroofs <afernandezs@uxcale.com>
2024-07-30 13:21:09 +0200

    Apartado 11 finalizado

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 //Crear forms en paginas web:
 //Un form es un conjunto de inputs para obtener datos del propio usuario;
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
 //Con el useForm, podremos generar muchas cosas diferentes relacionadas con los forms
 
 import * as yup from 'yup';
@@ -8,17 +9,23 @@ import * as yup from 'yup';
 //Con yup, vamos a validar la informacion que vamos a añadir en los forms.
 
 export const Form = () => {
-    const {register, handleSubmit } = useForm();
-
-    
+    //Con este componente "schema" indicaremos como queremos los datos; En el caso de required(), es porque le obligamos a meterlo.
     const schema = yup.object().shape({
-        fullName: yup.string().required(),
+        fullName: yup.string().required("Your Full Name is Required!"),
         email: yup.string().email().required(),
         age: yup.number().positive().integer().min(18).required(),
-        
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup
+            .string()
+            .oneOf([yup.ref("password"), null], "The confirm password is not the same as the previous password!") //Con esta parte, decimos que si o si tiene que ser el password anterior
+            .required(),
     })
 
-
+    const { register, handleSubmit, formState:{errors} } = useForm({
+        //Instalaremos otro paquete con el comando: npm install @hookform/resolvers y la declaramos en la zona de los imports arriba
+        resolver: yupResolver(schema),
+        //De esta forma, indicaremos que la forma de nuestro form, sera la de la funcion schema (con todas las restricciones que hemos añadido en cada campo)
+    });
 
     //Con onSubmit, tiraremos un mensaje en este caso cuando se le de clic en el boton submit
     const onSubmit = (data) => {
@@ -36,11 +43,18 @@ export const Form = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placerholder="Full Name..." {...register("fullName")}/>
+            <p>{errors.fullName?.message}</p>
             <input type="text" placerholder="Email..." {...register("email")}/>
+            <p>{errors.email?.message}</p>
             <input type="number" placerholder="Age..." {...register("age")}/>
+            <p>{errors.age?.message}</p>
             <input type="password" placerholder="Password..." {...register("password")}/>
+            <p>{errors.password?.message}</p>
             <input type="password" placerholder="Confirm Password..." {...register("confirmPassword")}/>
+            <p>{errors.confirmPassword?.message}</p>
             <input type="submit" />
         </form>
     );
 };
+
+//Los errores salen en rojo, porque en el app.css hemos creado un .p que basicamente lo convierte el color rojo
